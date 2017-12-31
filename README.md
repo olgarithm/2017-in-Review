7a46a09dc88279d40ec1dd8a59a98d553fa9bb2d
olgatron9000 <olga.andreeva@hotmail.com>
2017-12-30 21:04:12 -0800

    Cleaned up files

--- README.md
+++ README.md
@@ -2,28 +2,28 @@
 
 This is just a fun lil project I did over winter break to recap things I did in 2017.
 
-If you want to analyze your own Facebook data with the script that I made, here's what you need to do: 
+If you want to analyze your own Facebook data with the script that I made (react.js), here's what you need to do: 
 
 ## Let's Get Analyzing
 Step 1- Open up your profile
 
-![profile](screenshots/profile.png)
+![profile](images/screenshots/profile.png)
 
 Step 2- Open up your activity log
 
-![view activity log](screenshots/viewActivityLog.png)
+![view activity log](images/screenshots/viewActivityLog.png)
 
 Step 3- Make sure the activity log filter is highlighted
 
-![activity log](screenshots/activityLog.png)
+![activity log](images/screenshots/activityLog.png)
 
 Step 4- Start scrolling! Don't stop scrolling until you hit December 2016. My script only analyzes your interactions in 2017, so if you want to go further, you'll have to edit the script
 
-![december 2016](screenshots/december2016.png)
+![december 2016](images/screenshots/december2016.png)
 
 Step 5- Left click on the page (two finger tap on Mac OS), and select "Inspect element"
 
-![inspect element](screenshots/inspectElement.png)
+![inspect element](images/screenshots/inspectElement.png)
 
 Step 6- You should now see some HTML! Navigate to the line that says 
 
@@ -33,26 +33,28 @@ Step 6- You should now see some HTML! Navigate to the line that says
 
 and left tap on it. Then select "Expand all"
 
-![expand all](screenshots/expandAll.png)
+![expand all](images/screenshots/expandAll.png)
 
 Step 7- Now, we want to copy all of this HTML. Navigate back to the same line, left tap on it, and now select "Edit as HTML."
 
-![edit as HTML](screenshots/editAsHTML.png)
+![edit as HTML](images/screenshots/editAsHTML.png)
 
-Step 8- We're almost there! Copy and paste all of the HTML into your favorite text editor and create a new file called "sample.HTML"
+Step 8- We're almost there! Copy and paste all of the HTML into your favorite text editor and create a new file called "sample.html"
 
-![HTML](screenshots/HTML.png)
+![HTML](images/screenshots/HTML.png)
 
 Step 8.5- If you don't already have [node.js](https://nodejs.org/en/) installed on your computer, go download it! 
 
-Step 9- Here comes the fun part! Open up your terminal, navigate to the folder that contains both "sample.html" and "reacts.js," and type in 
+Step 9- Here comes the fun part! Download "reacts.js" and put it in the same folder as "sample.html." Then, open up your terminal, navigate to that folder, and type in 
 
 ```
-node.js react > results.txt
+node.js reacts > results.txt
 ```
 
-(This basically runs the code in react.js on sample.html and places the output into results.txt)
+(This basically runs the code in reacts.js on sample.html and places the output into results.txt)
 
 Step 10- Observe your data! The file "results.txt" should now contain a breakdown of your Facebook interactions in 2017!
 
-![results](screenshots/results.png)
+![results](images/screenshots/results.png)
+
+**DISCLAIMER** What you do with this data is your own responsibility. I asked for permission from all of the people whose names appeared in my website. Don't do anything that compromises someone's privacy.
--- reacts.js
+++ reacts.js
@@ -8,98 +8,97 @@
 // cry - https://www.facebook.com/rsrc.php/v3/yk/r/x-r8xo-ZCcu.png
 // angry - https://www.facebook.com/rsrc.php/v3/yz/r/XTeRB5Z20Am.png
 
-var cheerio = require('cheerio'),
-    fs = require('fs'),
-    data = fs.readFileSync('sample.html', 'utf8'),
-    $ = cheerio.load(data);
-    var images = ["https://www.facebook.com/rsrc.php/v3/yB/r/lDwm6Y_i0v8.png", "https://www.facebook.com/rsrc.php/v3/yn/r/Q2ZsBFJIdXg.png", "https://www.facebook.com/rsrc.php/v3/yX/r/85Fysyalo_E.png", "https://www.facebook.com/rsrc.php/v3/yT/r/fhpn7HuBJXG.png", "https://www.facebook.com/rsrc.php/v3/yk/r/x-r8xo-ZCcu.png", "https://www.facebook.com/rsrc.php/v3/yz/r/XTeRB5Z20Am.png"];
-    var reactions = [0, 0, 0, 0, 0, 0];
-	var months = ["Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"];
-	var friendsMadePerMonth = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0];
-	var totalFriends = 0;
-	var friends = new Map();
+var cheerio = require('cheerio')
+var fs = require('fs')
+var data = fs.readFileSync('sample.html', 'utf8'),
+$ = cheerio.load(data);
+var images = ["https://www.facebook.com/rsrc.php/v3/yB/r/lDwm6Y_i0v8.png", "https://www.facebook.com/rsrc.php/v3/yn/r/Q2ZsBFJIdXg.png", "https://www.facebook.com/rsrc.php/v3/yX/r/85Fysyalo_E.png", "https://www.facebook.com/rsrc.php/v3/yT/r/fhpn7HuBJXG.png", "https://www.facebook.com/rsrc.php/v3/yk/r/x-r8xo-ZCcu.png", "https://www.facebook.com/rsrc.php/v3/yz/r/XTeRB5Z20Am.png"];
+var reactions = [0, 0, 0, 0, 0, 0];
+var months = ["Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"];
+var friendsMadePerMonth = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0];
+var totalFriends = 0;
+var friends = new Map();
 
-	// Only looks at interactions that happened in 2017
-	// Searches through the HTML and does three things:
-	// 		1) Compares the image of the activity to the like/love/laugh/wow/cry/angry images
-	//			a) If it matches one of the images, increments that index of the reactions array
-	//		2) Finds all the friends I made and increments that month's index of the friendsMadePerMonth array 
-	//		3) Gets the name of friends whose content I either commented on or liked/reacted to
-	//			a) Puts that name into a map: Key -> name, Value -> # of times I've interacted with them
-	//			b) Sort the map by # of times I've interacted with them
-    $('div[id="year_2017"]').each(function() {
-    	$(this).find('div[class="clearfix"]').each(function(i, elem) {
-    		var stringedHTML = String(($(this).html()));
-    		for (var j = 0; j < images.length; j++) {
-    			if (stringedHTML.includes(images[j])) {
-    				reactions[j]++;
+// Only looks at interactions that happened in 2017
+// Searches through the HTML and does three things:
+// 		1) Compares the image of the activity to the like/love/laugh/wow/cry/angry images
+//			a) If it matches one of the images, increments that index of the reactions array
+//		2) Finds all the friends I made and increments that month's index of the friendsMadePerMonth array 
+//		3) Gets the name of friends whose content I either commented on or liked/reacted to
+//			a) Puts that name into a map: Key -> name, Value -> # of times I've interacted with them
+//			b) Sort the map by # of times I've interacted with them
+$('div[id="year_2017"]').each(function() {
+	$(this).find('div[class="clearfix"]').each(function(i, elem) {
+		var stringedHTML = String(($(this).html()));
+		for (var j = 0; j < images.length; j++) {
+			if (stringedHTML.includes(images[j])) {
+				reactions[j]++;
+			} 
+		}
+
+		// If we find an accepted friend request, we want to increment the
+		// friendsMadePerMonth array appropriately
+		if (stringedHTML.includes("became friends with")) {
+			for (var j = 0; j < months.length; j++) {
+				if (stringedHTML.includes(months[j])) {
+    				friendsMadePerMonth[j]++;
+    				totalFriends++;
     			} 
-    		}
+			}
+		}
 
-    		// If we find an accepted friend request, we want to increment the
-    		// friendsMadePerMonth array appropriately
-    		if (stringedHTML.includes("became friends with")) {
-				for (var j = 0; j < months.length; j++) {
-					if (stringedHTML.includes(months[j])) {
-	    				friendsMadePerMonth[j]++;
-	    				totalFriends++;
-	    			} 
-				}
-    		}
-
-    		// Most activity will be in the form:
-    		// [YOUR NAME] likes [FRIEND NAME'S] post
-    		// [YOUR NAME] reacted to [FRIEND NAME'S] post
-    		// [YOUR NAME] commented on [FRIEND NAME'S] post
-    		// We extract the friend's name and get rid of the "'s" at the end
-    		var stringedText = $(this).text();
-    		var strings = stringedText.split(" ");
-    		if(stringedText.indexOf("likes") > 0 || stringedText.indexOf("liked") > 0) {
-    			var friendsName = strings[3] + " " + strings[4].substring(0, strings[4].length - 2);
+		// Most activity will be in the form:
+		// [YOUR NAME] likes [FRIEND NAME'S] post
+		// [YOUR NAME] reacted to [FRIEND NAME'S] post
+		// [YOUR NAME] commented on [FRIEND NAME'S] post
+		// We extract the friend's name and get rid of the "'s" at the end
+		var stringedText = $(this).text();
+		var strings = stringedText.split(" ");
+		if(stringedText.indexOf("likes") > 0 || stringedText.indexOf("liked") > 0) {
+			var friendsName = strings[3] + " " + strings[4].substring(0, strings[4].length - 2);
+			if (friends.get(friendsName) != undefined) {
+				friends.set(friendsName, friends.get(friendsName) + 1);
+			} else {
+				friends.set(friendsName, 1);
+			}
+		} else if(stringedText.indexOf("to") > 0) {
+			if (strings[5] != undefined) {
+    			var friendsName = strings[4] + " " + strings[5].substring(0, strings[5].length - 2);
     			if (friends.get(friendsName) != undefined) {
     				friends.set(friendsName, friends.get(friendsName) + 1);
     			} else {
     				friends.set(friendsName, 1);
     			}
-    		} else if(stringedText.indexOf("to") > 0) {
-    			if (strings[5] != undefined) {
-	    			var friendsName = strings[4] + " " + strings[5].substring(0, strings[5].length - 2);
-	    			if (friends.get(friendsName) != undefined) {
-	    				friends.set(friendsName, friends.get(friendsName) + 1);
-	    			} else {
-	    				friends.set(friendsName, 1);
-	    			}
-	    		}
-    		} else if(stringedText.indexOf("on") > 0) {
-    			if (strings[5] != undefined) {
-	    			var friendsName = strings[4] + " " + strings[5].substring(0, strings[5].length - 2);
-	    			if (friends.get(friendsName) != undefined) {
-	    				friends.set(friendsName, friends.get(friendsName) + 1);
-	    			} else {
-	    				friends.set(friendsName, 1);
-	    			}
-	    		}
     		}
-    	});
+		} else if(stringedText.indexOf("on") > 0) {
+			if (strings[5] != undefined) {
+    			var friendsName = strings[4] + " " + strings[5].substring(0, strings[5].length - 2);
+    			if (friends.get(friendsName) != undefined) {
+    				friends.set(friendsName, friends.get(friendsName) + 1);
+    			} else {
+    				friends.set(friendsName, 1);
+    			}
+    		}
+		}
+	});
 
-    	// Now that we have all the friends I've interacted with, I want to be able to sort them. 
-    	// So, I make a friend object and put it into an array so that I can sort it by value later.
-    	var friendsArray = [];
-    	friends.forEach(function(value, key, map) {
-  			var friendObject = new Object();
-  			friendObject.name = key;
-  			friendObject.value = value;
-  			friendsArray.push(friendObject);
-		});
+	// Now that we have all the friends I've interacted with, I want to be able to sort them. 
+	// So, I make a friend object and put it into an array so that I can sort it by value later.
+	var friendsArray = [];
+	friends.forEach(function(value, key, map) {
+			var friendObject = new Object();
+			friendObject.name = key;
+			friendObject.value = value;
+			friendsArray.push(friendObject);
+	});
 
-		friendsArray.sort(function(a,b) {
-    		return b.value - a.value;
-		});
+	friendsArray.sort(function(a,b) {
+		return b.value - a.value;
+	});
 
-    	console.log("Like, love, laugh, wow, cry, angry reactions: " + "[" + reactions + "]");
-    	console.log("Friends made per month: " + "[" + friendsMadePerMonth + "]");
-    	console.log("Total friends made in 2017: " + totalFriends);
-    	console.log("How much you interact with your friends:");
-    	console.log(friendsArray);
-    });
-  
+	console.log("Like, love, laugh, wow, cry, angry reactions: " + "[" + reactions + "]");
+	console.log("Friends made per month: " + "[" + friendsMadePerMonth + "]");
+	console.log("Total friends made in 2017: " + totalFriends);
+	console.log("How much you interact with your friends:");
+	console.log(friendsArray);
+});
